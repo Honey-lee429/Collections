54c0c695e9fd81f433bdb018f596decfb45a7f5f
Hanna <79318688+Honey-lee429@users.noreply.github.com>
2024-03-20 07:48:15 -0300

    Create Colecao.java

---
+++ collections/Colecao.java
@@ -0,0 +1,10 @@
+O Collections Framework é uma estrutura bem definida composta por um conjunto de interfaces e classes que servem para representar 
+  e tratar grupos de dados como uma única entidade, que é comumente chamada de coleção. Dentro do Collections Framework, encontramos 
+  os seguintes elementos:
+
+Interfaces: Permitem a manipulação das coleções seguindo o princípio de “Programar para interfaces e não para implementações”,
+  o que significa que o acesso aos objetos deve ser feito apenas por meio dos métodos definidos nessas interfaces.
+Implementações: Referem-se às implementações concretas das interfaces. Estas são as classes que fornecem uma implementação 
+  real das interfaces e são usadas para criar instâncias específicas de coleções.
+Algoritmos: São métodos que executam diversas operações sobre os objetos contidos nas coleções, incluindo operações 
+  como busca e ordenação.
